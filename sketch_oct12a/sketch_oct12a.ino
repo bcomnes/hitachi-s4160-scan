fcb75f683c1a0ff2612f27a27804d91b83f88fef
Bret Comnes <bcomnes@gmail.com>
2012-10-17 13:51:59 -0700

    Initial program write, needs testing
    It should basically work, but I need to test 
    this on an actual Arduino.  
    
    Right now, it should 0 the stage, take data, then move a step, sending the data to the 
    computer via serial.
    
    Signed-off-by: Bret Comnes <bcomnes@gmail.com>

--- sketch_oct12a/sketch_oct12a.ino
+++ sketch_oct12a/sketch_oct12a.ino
@@ -1,25 +1,36 @@
 /*
-  AnalogReadSerial
-  Reads an analog input on pin 0, prints the result to the serial monitor.
-  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
- 
- This example code is in the public domain.
+A quick program to to posotion the stage of the SEM, take data, and 
+output it to the serial line to the computer.
  */
- // Code examples from:
- // http://arduino.cc/en/Tutorial/ReadAnalogVoltage
 
+int sensorPin = A0; // This is the pin we will read out signal voltage with
+int xPin = 9;   // Sets out x position pin
+int yPin = 10;   // This sets up out y position pin
+int sensorValue = 0; // Initializing our sensor value variable 
+int settle_ms = 10;  //This is the settle time after each xy pos. change
+int x0 = 0; // Our initial x position
+int y0 = 0; // Our initial y position
+int stageStep = 1; //Our step size for our position stage 
 
-// the setup routine runs once when you press reset:
 void setup() {
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
+	pinMode(xPin, OUTPUT); // declare the xPin as an PWM OUTPUT
+	pinMode(yPin, OUTPUT); // declare the yPin as an PWM OUTPUT:
+	Serial.begin(9600);  //Sets up a serial output to our computer at 9600 bps
+	delay(settle_ms);
+	
+	for (int y = y0 ; y <= 255; y +=stageStep) {  //For y = 0 to 255
+		analogWrite(yPin, y); // Move the state to t
+		for (int x = x0 ; x <= 255; x +=stageStep) { //For x = to 255
+		    analogWrite(xPin, x); // Move the stage to x
+			delay(settle_ms); // Wait for the motion to settle
+			sensorValue = analogRead(sensorPin); // Read the image signal
+		    Serial.print(sensorValue; // Send the value to the computer
+		}
+		Serial.print("\t");  // Prints a new line when we move y
+	}
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
-  // read the input on analog pin 0:
-  int sensorValue = analogRead(A0);
-  // print out the value you read:
-  Serial.println(sensorValue);
-  delay(1);        // delay in between reads for stability
+	// When the data is collected, stop everything. 
+	// Press reset on the computer if you want to run again.	
 }
