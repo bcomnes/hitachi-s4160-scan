2c83586bf2c041c1ab14a42e312e66c2468cc424
Bret Comnes <bcomnes@gmail.com>
2012-10-18 19:08:00 -0700

    Before wrapper functions
    Made a bunch of edits while working on PWM.
    
    
    Signed-off-by: Bret Comnes <bcomnes@gmail.com>

--- scan.ino
+++ scan.ino
@@ -5,84 +5,97 @@ output it to the serial line to the computer.
  */
  
  // Pin assignments
-int pin_sensor = A0; // This is the pin we will read out signal voltage with
-int pin_scan_x = 9;  // Sets out x position pin
-int pin_scan_y = 10; // This sets up out y position pin
-int pin_enable_x = 1; // This sets our x scan enable pin
-int pin_enable_y = 2; // This sets out y scan enable pin
+int pin_sensor                  = A0; // This is the pin we will read out signal voltage with
+int pin_scan_x                  = 9;  // Sets out x position pin
+int pin_scan_y                  = 10; // This sets up out y position pin
+
+      int pin_blanking          = 1; 
+const int pin_blanking_on       = LOW;
+const int pin_blanking_off      = HIGH;
 
 // Variables
-volatile int sensorValue = 0; // Initializing our sensor value variable 
-volatile int settle_x_ms = 100;  // This is the settle time after each xy pos. change
-volatile int settle_y_ms = 100;  // This is the settle time after each xy pos. change
-volatile int settle_initial_ms = 5000; // This allows us to delay out program from running
-volatile int settle_rescan_x_ms = 500; // Rescan settle time x
-volatile int settle_rescan_y_ms = 500; // Rescan settle time y
+volatile int sensorValue        = 0; // Initializing our sensor value variable 
+volatile int settle_x_ms        = 1;  // This is the settle time after each xy pos. change
+volatile int settle_y_ms        = 1;  // This is the settle time after each xy pos. change
+volatile int settle_initial_ms  = 0; // This allows us to delay out program from running
+volatile int settle_retrace_x_ms = 1; // retrace settle time x
+volatile int settle_retrace_y_ms = 1; // retrace settle time y
 
-volatile int scan_x_start = 0; // The PWM x initial value
-volatile int scan_y_start = 0; // The PWM y initial value
-volatile int scan_x_stop = 255; // The PWM x final value
-volatile int scan_y_stop = 255; // The PWM y final value
-volatile int scan_x_step = 1; // Our x step size for our position stage
-volatile int scan_y_step = 1; // Our y step size for our position stage
+volatile int scan_x_start       = 0; // The PWM x initial value
+volatile int scan_y_start       = 0; // The PWM y initial value
+volatile int scan_x_stop        = 255; // The PWM x final value
+volatile int scan_y_stop        = 10; // The PWM y final value
+volatile int scan_x_step        = 1; // Our x step size for our position stage
+volatile int scan_y_step        = 1; // Our y step size for our position stage
 
-volatile int enable_x = LOW; //Initial state for our x enable pin
-volatile int enable_y = LOW; //Initial state for our y enable pin
+//volatile int blanking = HIGH;
 
 //#####################################################################
 
 void setup() 
-{// This is treated as out main loop for now
+{// This is treated as out main() function for now
 	pinMode(pin_scan_x, OUTPUT); // declare the xPin as an PWM OUTPUT
 	pinMode(pin_scan_y, OUTPUT); // declare the yPin as an PWM OUTPUT:
 //	pinMode(pin_sensor, INPUT);  // Might be importaint %TODO%
-	pinMode(scan_x_step, OUTPUT); // Sets up our x enable pin
-	pinMode(scan_y_step, OUTPUT); // Sets up our y enable pin
+
+
+	pinMode(      pin_blanking, OUTPUT );	
+	digitalWrite( pin_blanking, pin_blanking_on   );
 	
 	Serial.begin(9600);  //Sets up a serial output to our computer at 9600 bps
 	
 	delay(settle_initial_ms); // Wait an initial ammount of time before starting
 	
 	// This puts usinto the correct position, and settles before beginig our loop
-	enable_x = HIGH; 
-	enable_y = HIGH;
 	analogWrite(pin_scan_x, scan_x_start);
-	analogWrite(pin_scan_y, scan_x_start);
-	delay(settle_rescan_x_ms);
-	delay(settle_rescan_y_ms);
+	analogWrite(pin_scan_y, scan_y_start);
+	delay(settle_retrace_x_ms);
+	delay(settle_retrace_y_ms);
+	
+	Serial.print("Seconds, Hexfrac, Board_ID, Rec_Null, \r\n");
+	Serial.print("Seconds, Hexfrac, Board_ID, Rec_Null, \r\n"); 
+	Serial.print("Seconds, Hexfrac, Board_ID, Rec_Null, \r\n");
+	Serial.print("Seconds, Hexfrac, Board_ID, Rec_Scan_Start, \r\n");
 	
 	
 	for (int y = scan_y_start ; y <= scan_y_stop ; y +=scan_y_step)
 		{  // Outerloop(y)
-	    
+			
 		Serial.print("Seconds, "); 
 		Serial.print("Hexfrac, "); 
 		Serial.print("Board_ID, ");
-		Serial.print("Rec_Format, "); 				
-	    Serial.print(y);
-		Serial.print(", Bloop"); // Send the value to the computer			
+		Serial.print("Rec_Format, ");
+		Serial.print(y);
+		Serial.print(", Bloop"); // Send the value to the computer
 		
 		analogWrite(pin_scan_y, y); // Move the state to t
 		delay(settle_y_ms);
 		
+		digitalWrite(pin_blanking, pin_blanking_off);
 		for (int x = scan_x_start ; x <= scan_x_stop ; x +=scan_x_step)
 			{ // Innerloop(x)
 			analogWrite(pin_scan_x, x); // Move the stage to x
 			delay(settle_x_ms); // Wait for the motion to settle
 			sensorValue = analogRead(pin_sensor); // Read the image signal
-			Serial.print(", "); // Send the value to the computer			
+			Serial.print(", "); // Send the value to the computer
 			Serial.print(sensorValue); // Send the value to the computer
 			}
-			
-		Serial.print("\n");  // Prints a new line when we move y
+		digitalWrite(pin_blanking, pin_blanking_on);
+		
+		Serial.print("\r\n");  // Prints a new line when we move y
 		
 		analogWrite(pin_scan_x, scan_x_start);
-		delay(settle_rescan_x_ms);
+		delay(settle_retrace_x_ms);
 		}
+
+
+
+	Serial.print("Seconds, Hexfrac, Board_ID, Rec_Scan_Complete, \r\n");
+	Serial.print("Seconds, Hexfrac, Board_ID, Rec_Null, \r\n");
+	Serial.print("Seconds, Hexfrac, Board_ID, Rec_Null, \r\n"); 
+	Serial.print("Seconds, Hexfrac, Board_ID, Rec_Null, \r\n");
 	analogWrite(pin_scan_y, scan_y_start);
-	delay(settle_rescan_y_ms);
-    enable_x = LOW; 
-	enable_y = LOW;
+	delay(settle_retrace_y_ms);
 }
 //#####################################################################
 void loop() {
